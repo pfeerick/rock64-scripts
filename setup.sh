9ae7b4cb40891dcf3a272ea3ec299ba7fbcceeff
Peter Feerick <peter.feerick@gmail.com>
2017-06-26 04:46:58 +0000

    Typos forgot to fix and uncommented sections for use

--- setup.sh
+++ setup.sh
@@ -8,17 +8,17 @@ if [ "$(id -u)" -ne "0" ]; then
 fi
 
 #add my username
-#adduser pfeerick
-#usermod -aG sudo pfeerick
+adduser pfeerick
+usermod -aG sudo pfeerick
 
 #update system
-#apt-get update && apt-get upgrade
+apt-get update && apt-get upgrade
 
 #add network manager?
-#apt-get instal --no-install-recommends network-manager
+#apt-get install --no-install-recommends network-manager
 
 #restore ssh key backup from server
-#wget -qO- 192.168.0.5/ssh-backup/rock64.tar.gz | tar -zvxf - -C /
+wget -qO- 192.168.0.5/ssh-backup/rock64.tar.gz | tar -zvxf - -C /
 
 #get git repo up and running again
-sudo -u pfeerick bash -c "mkdir /home/pfeerick/repos/rock64-scripts && git clone git@github.com:pfeerick/rock64-scripts.git /home/pfeerick/repos/rock64-scripts"
+sudo -u pfeerick bash -c "mkdir -p /home/pfeerick/repos/rock64-scripts && git clone git@github.com:pfeerick/rock64-scripts.git /home/pfeerick/repos/rock64-scripts"
