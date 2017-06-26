6a4e0a6bc9804ba054bf926f77fb4e0ee0e3d768
Peter Feerick <peter.feerick@gmail.com>
2017-06-26 04:43:20 +0000

    Added script to provision rock64 when changing between images

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+if [ "$(id -u)" -ne "0" ]; then
+        echo "This script requires root."
+        exit 1
+fi
+
+#add my username
+#adduser pfeerick
+#usermod -aG sudo pfeerick
+
+#update system
+#apt-get update && apt-get upgrade
+
+#add network manager?
+#apt-get instal --no-install-recommends network-manager
+
+#restore ssh key backup from server
+#wget -qO- 192.168.0.5/ssh-backup/rock64.tar.gz | tar -zvxf - -C /
+
+#get git repo up and running again
+sudo -u pfeerick bash -c "mkdir /home/pfeerick/repos/rock64-scripts && git clone git@github.com:pfeerick/rock64-scripts.git /home/pfeerick/repos/rock64-scripts"
