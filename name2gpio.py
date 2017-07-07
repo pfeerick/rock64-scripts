e8223198c1deb83cf4b8cbf015c22294ab8db4dd
Peter Feerick <peter.feerick@gmail.com>
2017-07-07 14:40:39 +1000

    Changes by Marcus to take into account GPIO banks

--- name2gpio.py
+++ name2gpio.py
@@ -7,18 +7,20 @@
 import sys
 import string
 
+pads={"A":0,"B":1,"C":2,"D":3}
+
 def convert(value):
-    value = value.upper()
-    bank_num = int(value[4:5], 10)
-    reg_num = int(value[7:], 10)
-    gpionum = (bank_num * 32) + reg_num
-    return gpionum
+   value = value.upper()
+   bank_num = int(value[4:5], 10)
+   pad_nam = pads[value[6:7]]
+   pad_num = int(value[7:], 10)
+   gpionum = (bank_num * 32) + (pad_nam * 8) + pad_num
+   return gpionum
 
 if __name__ == "__main__":
-    args = sys.argv[1:]
-    if not args:
-        print("Usage: %s <bank_reg>  ie; GPIO3_A5" % sys.argv[0])
-        sys.exit(1)
+   args = sys.argv[1:]
+   if not args:
+       print("Usage: %s <bank_reg>  ie; GPIO3_A5" % sys.argv[0])
+       sys.exit(1)
 
-    print("%d" % convert(args[0]))
-    
+   print("%d" % convert(args[0]))
