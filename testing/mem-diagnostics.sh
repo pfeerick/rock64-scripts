66b399b7a0b9f1c453caec17be131f08812b2e94
Peter <pfeerick@users.noreply.github.com>
2018-05-28 11:29:26 +1000

    Create mem-diagnostics.sh
    Quick script to test the kernel for some memory issues

---
+++ testing/mem-diagnostics.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+MEM_TEST_MASK=6114
+MEM_TEST_SIZE=3GB
+MEM_TEST_LOOPS=2
+
+Main() {
+	s=$(date +%s)
+
+	RequireRoot
+	CheckDependencies
+
+	LogFile="$(mktemp /tmp/${0##*/}.XXXXXX)"
+
+	echo -e "### kernel: \n$(uname -a)" >> ${LogFile}
+
+	echo -e "\n### linux-rock64-package version: \n$(apt-cache policy linux-rock64-package | grep "Installed")" >> ${LogFile}
+
+	echo -e "\n### memtester (mask: ${MEM_TEST_MASK}, size: ${MEM_TEST_SIZE}, loops: ${MEM_TEST_LOOPS}):\n" >> ${LogFile}
+	m=$(date +%s)
+	MEMTESTER_TEST_MASK=${MEM_TEST_MASK} memtester ${MEM_TEST_SIZE} ${MEM_TEST_LOOPS} 2>&1 | tee -a ${LogFile}
+	echo -e "memtester took $[$(date +%s)-$m] seconds.\n" | tee -a ${LogFile}
+
+	echo -e "\n### dmesg:\n$(dmesg)" >> ${LogFile}
+	fping ix.io 2>/dev/null | grep -q alive || \
+				(echo -e "\nNetwork/firewall problem detected. Not able to upload debug info.\nPlease fix this or upload ${LogFile} manually\n" >&2 ; exit 1)
+	echo -e "Debug information will now be uploaded to \c"
+	# we obfuscate IPv4 addresses somehow but not too much, MAC addresses have to remain
+	# in clear since otherwise the log becomes worthless due to randomly generated
+	# addresses here and there that might conflict
+	cat ${LogFile} \
+		| sed -E 's/([0-9]{1,3}\.)([0-9]{1,3}\.)([0-9]{1,3}\.)([0-9]{1,3})/XXX.XXX.\3\4/g' \
+		| curl -F 'f:1=<-' ix.io
+	echo -e "Please post the URL in the forum where you've been asked for it.\n"
+
+	echo -e "Tests complete. Total elapsed time $[$(date +%s)-$m] seconds.\n"
+	exit 0
+} # Main
+
+RequireRoot() {
+	if [ "$(id -u)" != "0" ]; then
+		echo "This script requires root privleges - run as root or through sudo. Exiting" >&2
+		exit 1
+	fi
+} # RequireRoot
+
+CheckDependencies() {
+	MissingTools=""
+
+	which memtester >/dev/null 2>&1 || MissingTools="${MissingTools} memtester"
+	which curl >/dev/null 2>&1      || MissingTools="${MissingTools} curl"
+	which fping >/dev/null 2>&1     || MissingTools="${MissingTools} fping"
+
+	if [ "X${MissingTools}" != "X" ]; then
+		echo -e "Some tools are missing, installing: ${MissingTools}" >&2
+		apt-get -f -qq -y install ${MissingTools} >/dev/null 2>&1
+	fi
+} # CheckDependencies
+
+Main "$@"
